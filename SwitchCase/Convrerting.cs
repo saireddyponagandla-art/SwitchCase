cc302ab212078dd45474fca0457d023c97ae858d
saire <saire@SAIREDDY_2005>
2026-03-21 12:55:14 +0530

    Revert "change"
    This reverts commit c1e57be2f420bc3a6e225e10f0ce44a476f511fe.

--- SwitchCase/Convrerting.cs
+++ SwitchCase/Convrerting.cs
@@ -12,12 +12,12 @@ namespace SwitchCase
         {
             // string a = " Rama";
             // int b = Convert.ToInt32(a);
-            float b = 68f;
+            float b = 14.0f;
             char c = (char)b;
             Console.WriteLine(c);
-            float  C = 66.0f;
-            char d = (char)C;
-            Console.WriteLine(d);
+            float  c = 66.0f;
+            char d = (char)c;
+            console.WriteLine(d);
 
         }
         
--- SwitchCase/Program.cs
+++ SwitchCase/Program.cs
@@ -7,58 +7,58 @@ using System.Threading.Tasks;
 
 namespace Switch
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the Month Number :");
-            int Month = Convert.ToInt32(Console.ReadLine());
+   internal class Program
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("Enter the Month Number :");
+           int Month = Convert.ToInt32(Console.ReadLine());
 
-            switch (Month)
-            {
-                case 1:
-                    Console.WriteLine("January");
-                    break;
-                case 2:
-                    Console.WriteLine("Febrary");
-                    break;
-                case 3:
-                    Console.WriteLine(" march");
-                    break;
-                case 4:
-                    Console.WriteLine("april");
-                    break;
-                case 5:
-                    Console.WriteLine("may");
-                    break;
-                case 6:
-                    Console.WriteLine("june");
-                    break;
-                case 7:
-                    Console.WriteLine("july");
-                    break;
-                case 8:
-                    Console.WriteLine("August");
-                    break;
-                case 9:
-                    Console.WriteLine("september");
-                    break;
-                case 10:
-                    Console.WriteLine("october");
-                    break;
-                case 11:
-                    Console.WriteLine("Novevber");
-                    break;
-                case 12:
-                    Console.WriteLine("december");
-                    break;
-                default:
-                    Console.WriteLine(" Please enter vaild Number :");
-                    break;
+           switch (Month)
+           {
+               case 1:
+                   Console.WriteLine("January");
+                   break;
+               case 2:
+                   Console.WriteLine("Febrary");
+                   break;
+               case 3:
+                   Console.WriteLine(" march");
+                   break;
+               case 4:
+                   Console.WriteLine("april");
+                   break;
+               case 5:
+                   Console.WriteLine("may");
+                   break;
+               case 6:
+                   Console.WriteLine("june");
+                   break;
+               case 7:
+                   Console.WriteLine("july");
+                   break;
+               case 8:
+                   Console.WriteLine("August");
+                   break;
+               case 9:
+                   Console.WriteLine("september");
+                   break;
+               case 10:
+                   Console.WriteLine("october");
+                   break;
+               case 11:
+                   Console.WriteLine("Novevber");
+                   break;
+               case 12:
+                   Console.WriteLine("december");
+                   break;
+               default:
+                   Console.WriteLine(" Please enter vaild Number :");
+                   break;
 
 
 
-            }
-        }
-    }
+           }
+       }
+   }
 }
